807e8e8b4ac50fd2405a8d02c8fec46cd2498413
zll <598879827@qq.com>
2020-08-02 11:53:49 +0800

    commit initialize

---
+++ 市值优化回测代码整理/utils.py
@@ -0,0 +1,123 @@
+from typing import Mapping
+import pandas as pd
+import rqdatac as rqd
+import pickle
+
+rqd.init()
+
+
+INDUSTRY_SHENWAN_TOP_10 = [
+    '801890.INDX', '801030.INDX', '801150.INDX', '801080.INDX', '801750.INDX',
+    '801730.INDX', '801880.INDX', '801760.INDX', '801160.INDX', '801140.INDX'
+]
+
+INDUSTRY_SHENWAN = [
+    '801010.INDX', '801020.INDX', '801030.INDX', '801040.INDX', '801050.INDX', '801080.INDX', '801110.INDX',
+    '801120.INDX', '801130.INDX', '801140.INDX', '801150.INDX', '801160.INDX', '801170.INDX', '801180.INDX',
+    '801200.INDX', '801210.INDX', '801230.INDX', '801710.INDX', '801720.INDX', '801730.INDX', '801740.INDX',
+    '801750.INDX', '801760.INDX', '801770.INDX', '801780.INDX', '801790.INDX', '801880.INDX', '801890.INDX',
+]
+
+INDUSTRY_ZHONGXIN = [
+    'CI005001.WI', 'CI005002.WI', 'CI005003.WI', 'CI005004.WI', 'CI005005.WI', 'CI005006.WI', 'CI005007.WI',
+    'CI005008.WI', 'CI005009.WI', 'CI005010.WI', 'CI005011.WI', 'CI005012.WI', 'CI005013.WI', 'CI005014.WI',
+    'CI005015.WI', 'CI005016.WI', 'CI005017.WI', 'CI005018.WI', 'CI005019.WI', 'CI005020.WI', 'CI005021.WI',
+    'CI005022.WI', 'CI005023.WI', 'CI005024.WI', 'CI005025.WI', 'CI005026.WI', 'CI005027.WI', 'CI005028.WI',
+    'CI005029.WI', 'CI005030.WI'
+]
+
+
+# def get_universe(date):
+#     return (
+#         rqd.all_instruments(type='CS', date=date)
+#             .loc[lambda df: ~df['status'].isin(['Delisted', 'Unknown'])]
+#             .loc[:, 'order_book_id']
+#             .tolist()
+#     )
+
+
+def get_industry(universe, date):
+    return (
+        rqd.shenwan_instrument_industry(universe, date).loc[:, 'index_name'].rename('industry')
+    )
+
+
+def drop_st(universe, date):
+    is_st = rqd.is_st_stock(universe, date, date).squeeze()
+    assert isinstance(is_st, pd.Series), 'is_st is not series'
+    return is_st.index[~is_st]
+
+
+def drop_suspended(universe, date):
+    # squeeze(axis=0):DataFrames with a single column or a single row are squeezed to a Series
+    is_suspended = rqd.is_suspended(universe, date, date).squeeze()
+    assert isinstance(is_suspended, pd.Series), 'is_suspended is not series'
+    return is_suspended.index[~is_suspended]
+
+
+def drop_recently_listed(universe, date, min_days_listed=60):
+    # 一个列表,里面元素是Instrument对象
+    instruments = rqd.instruments(universe)
+    return [inst.order_book_id
+            for inst in instruments
+            if inst.days_from_listed(date) > min_days_listed]
+
+
+def select_top_N_percent(universe, date, scorer, n, ascending, min_selected, grouper):
+    """选前N%的股票"""
+    if isinstance(scorer, Mapping):
+        scores = pd.Series(scorer)
+    elif isinstance(scorer, pd.Series):
+        scores = scorer
+    # scorer是个函数
+    elif callable(scorer):
+        scores = scorer(universe, date)
+    else:
+        raise ValueError(f'Unsupported scorer type: {type(scorer)}')
+
+    if not grouper:
+        # 排名,0-1的分位数
+        ranking = scores.rank(ascending=ascending, pct=True)
+        # 取 N 和最小比例的较大值
+        assert min_selected < len(scores), 'min_selected is less than len(scores)'
+        cutoff = max(n, min_selected / len(scores))
+        # 取排名较小的股票
+        selected = ranking[ranking <= cutoff].index
+        return selected
+    # 行业分组
+    else:
+        # grouping:一个分组的series
+        grouping = get_industry(universe, date)
+        selected = []
+        # 对每一组进行排序选股
+        for _, scores_of_group in scores.groupby(grouping):
+            group = scores_of_group.index
+            sel = select_top_N_percent(
+                group, date, scores_of_group, n, ascending, min_selected,
+                grouper=False
+            )
+            selected.extend(sel)
+        return selected
+
+
+_RICEQUANT_FACTORS = rqd.get_all_factor_names()
+
+_CACHE = {}  # factor -> pd.Series
+
+
+def _get_factor_from_local_file(universe, factor, start_date):
+    if factor in _CACHE:
+        return _CACHE[factor][start_date][universe]
+    else:
+        with open(f'{factor}.pkl', 'rb') as pf:
+            data = pickle.load(pf)
+        _CACHE[factor] = data
+        return data[start_date][universe]
+
+
+def get_factor(universe, factor, start_date, end_date):
+    assert start_date == end_date, 'start_date must equal to end_date'
+    if factor in _RICEQUANT_FACTORS:
+        return rqd.get_factor(universe, factor, start_date, end_date)
+    else:
+        return _get_factor_from_local_file(universe, factor, start_date)
---
+++ 成分股数前十大行业单因子测试/factor_backtest_multiprocessing.py
@@ -0,0 +1,182 @@
+import rqalpha as rqa
+import rqdatac as rqd
+import rqoptimizer as rqo
+import multiprocessing
+import utils
+import datetime
+import time
+import pickle
+import os
+
+rqd.init()
+
+
+def get_target_stocks(context, factor, ascending, percent_selected, min_selected, grouper):
+    """获得当天需要买入的股票"""
+    # 前一个交易日
+    selection_date = rqd.get_previous_trading_date(context.now)
+    # 前一个交易日的指数成分股
+    universe = rqd.index_components(context.index_stockpool, selection_date)
+    # 剔除当天停牌
+    universe = utils.drop_suspended(universe, selection_date)
+    # 剔除当天ST
+    universe = utils.drop_st(universe, selection_date)
+    # 剔除上市小于60天
+    universe = utils.drop_recently_listed(universe, selection_date, 60)
+    # 获得因子分数
+    scores = utils.get_factor(universe, factor, selection_date, selection_date)
+    # 返回需要买入的股票
+    return utils.select_top_N_percent(
+        universe, selection_date, scores, percent_selected, ascending, min_selected,
+        grouper=grouper,
+    )
+
+
+def get_target_portfolio(context, **optimization_args):
+    # 返回每只股票的权重Series
+    # rqoptimizer 默认取date前一个交易日的数据
+    target_portfolio = rqo.portfolio_optimize(
+        context.target_stocks, context.now, **optimization_args
+    )
+    return target_portfolio.loc[lambda x: x != 0]
+
+
+def init(context):
+    pass
+
+
+def handle_bar(context, bar_dict):
+    # 如果是False(不是月初),直接返回
+    if not _should_rebalance(context):
+        return
+    print(context.now)
+    print("现有持仓数:", len(context.portfolio.positions))
+    context.target_stocks = get_target_stocks(context, **context.stock_selection_args)
+    context.target_portfolio = get_target_portfolio(context, **context.optimization_args)
+    rebalance(context, bar_dict)
+
+
+def _should_rebalance(context):
+    """判断今天是否调仓"""
+    # 获得上一个交易日
+    prev_trading_day = rqd.get_previous_trading_date(context.now)
+    # 判断是否是月初, 是月初返回True
+    is_month_start = (prev_trading_day.month != context.now.month)
+    return is_month_start
+
+
+def rebalance(context, bar_dict):
+    # 先清空不在目标组合里面的股票
+    positions = context.stock_account.positions
+    for order_book_id in positions:
+        if order_book_id not in context.target_portfolio:
+            rqa.api.order_to(order_book_id, 0)
+
+    # 对每个股票计算目标权重和当前权重的差值
+    # 差值为正的是买单, 反之为卖单
+    capital = context.stock_account.total_value * (1 - context.cash_cushion)
+    to_sell, to_buy = {}, {}
+    for order_book_id, weight in context.target_portfolio.items():
+        # 股票目标价值
+        target_value = capital * weight
+        # 目标和现有之差
+        gap = target_value - positions[order_book_id].market_value
+        if gap > 0:
+            to_buy[order_book_id] = gap
+        else:
+            to_sell[order_book_id] = gap
+
+    # to avoid liquidity issue, sell first, buy second
+    _money_for_one_lot = lambda order_book_id: bar_dict[order_book_id].close * 100
+    # 先卖
+    for order_book_id, value in to_sell.items():
+        # 要卖出的至少大于1手股票价值
+        if abs(value) > _money_for_one_lot(order_book_id):
+            # value为负
+            rqa.api.order_value(order_book_id, value)
+    # 后买
+    for order_book_id, value in to_buy.items():
+        # 要买入的至少大于1手股票价值
+        if abs(value) > _money_for_one_lot(order_book_id):
+            rqa.api.order_value(order_book_id, value)
+
+
+def my_run(init, handle_bar, config):
+    _code = config['extra']['context_vars']['index_stockpool']
+    _factor = config['extra']['context_vars']['stock_selection_args']['factor']
+    backtest_results = rqa.run_func(init=init, handle_bar=handle_bar, config=config)
+    if not os.path.exists(f'results/{_code}'):
+        os.mkdir(f'results/{_code}')
+    with open(f'results/{_code}/{_factor}.pkl', 'wb') as pf:
+        pickle.dump((config, backtest_results), pf)
+
+
+if __name__ == '__main__':
+
+    MILLION = 1_000_000
+    BILLION = 1000 * MILLION
+
+    FACTORS = [
+        ('depreciation_per_share_ttm', True),
+        ('cash_equivalent_per_share_ttm', False),
+        ('adjusted_return_on_equity_ttm', False),
+        ('adjusted_return_on_equity_diluted_ttm', False),
+        ('ocf_to_current_ratio_ttm', False),
+        ('depreciation_and_amortization_ttm', False),
+        ('cash_equivalent_increase_growth_ratio_ttm', False)
+    ]
+
+    s = datetime.datetime.now()
+    for code in utils.INDUSTRY_SHENWAN_TOP_10:
+        process_list = []
+        for fac, asc in FACTORS:
+            config = {
+                "base": {
+                    "matching_type": "current_bar",
+                    "start_date": '2015-01-01',
+                    "end_date": '2020-07-20',
+                    "frequency": '1d',
+                    "accounts": {"stock": 0.1 * BILLION},
+                },
+
+                "mod": {
+                    "sys_analyser": {
+                        "enabled": True,
+                        "plot": True,
+                        "benchmark": code,
+                    },
+                },
+
+                "extra": {
+                    "log_level": 'error',
+                    "context_vars": {
+                        'cash_cushion': 0.005,
+                        'index_stockpool': code,
+                        'stock_selection_args': {
+                            'factor': fac,
+                            'ascending': asc,
+                            'percent_selected': 0.2,
+                            'min_selected': 15,
+                            'grouper': False
+                        },
+
+                        'optimization_args': {
+                            'benchmark': code,
+                            'objective': rqo.MinTrackingError(),
+                            'cons': [rqo.WildcardIndustryConstraint(lower_limit=-0.05, upper_limit=0.05, relative=True)]
+                        },
+                    },
+                },
+            }
+
+            p = multiprocessing.Process(target=my_run,
+                                        kwargs={'init': init, 'handle_bar': handle_bar,
+                                                'config': config}
+                                        )
+            p.start()
+            process_list.append(p)
+        for i in process_list:
+            i.join()
+
+    e = datetime.datetime.now()
+    print(f'共计用时: {e - s}')
